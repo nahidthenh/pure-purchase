05ead5ad5a7adfe32237fa4cfd5f4b1ccd5e6759
nahidthenh <nhgroup18@gmail.com>
2023-08-26 23:08:59 +0600

    SCSS added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
