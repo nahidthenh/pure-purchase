0aa9caa43c238200771243c7bbcdc381d370b02e
nahidthenh <nhgroup18@gmail.com>
2023-08-27 20:24:36 +0600

    Slider banner update

--- src/components/widget/Slider.jsx
+++ src/components/widget/Slider.jsx
@@ -10,12 +10,24 @@ import { Banner1, Banner2, Banner3, Banner4 } from '../../assets/image/banner/ba
 const Slider = () => {
     return (
         <div>
-            <Swiper effect={'fade'} spaceBetween={30} pagination={true} modules={[EffectFade, Pagination]}
+            <Swiper
+                effect={'fade'}
+                spaceBetween={30}
+                pagination={true}
+                modules={[EffectFade, Pagination]}
                 className="mySwiper">
-                <SwiperSlide className='slider-item'><img className='img-fluid' src={Banner2} alt="SLider image" /></SwiperSlide>
-                <SwiperSlide className='slider-item'><img className='img-fluid' src={Banner3} alt="SLider image" /></SwiperSlide>
-                <SwiperSlide className='slider-item'><img className='img-fluid' src={Banner4} alt="SLider image" /></SwiperSlide>
-                <SwiperSlide className='slider-item border'><img className='img-fluid' src={Banner1} alt="SLider image" /></SwiperSlide>
+                <SwiperSlide className='slider-item'>
+                    <img className='img-fluid' src={Banner2} alt="SLider image 1" />
+                </SwiperSlide>
+                <SwiperSlide className='slider-item'>
+                    <img className='img-fluid' src={Banner3} alt="SLider image 2" />
+                </SwiperSlide>
+                <SwiperSlide className='slider-item'>
+                    <img className='img-fluid' src={Banner4} alt="SLider image 3" />
+                </SwiperSlide>
+                <SwiperSlide className='slider-item border'>
+                    <img className='img-fluid' src={Banner1} alt="SLider image 4" />
+                </SwiperSlide>
             </Swiper>
         </div>
     );
