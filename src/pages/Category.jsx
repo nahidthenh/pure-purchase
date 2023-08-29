6e8be8f6d336858c437e405952662ddda767cb56
nahidthenh <nhgroup18@gmail.com>
2023-08-29 21:24:01 +0600

    Category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,38 @@
-
+import { Col, Container, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from './../utils/axios';
+import { Link } from "react-router-dom";
 
 const Category = () => {
+
+    const [categoryData, setCategoryData] = useState([])
+    useEffect(() => {
+        axios.get('/products/categories')
+            .then(res => {
+                setCategoryData(res.data);
+            })
+    }, [])
+
     return (
         <div className="mt-170">
-            <h3>This is Category</h3>
+            <Container>
+                <Row>
+                    <Col xl={12} lg={12} md={12} sm={12}>
+                        <h3 className="page-title">Categories</h3>
+                        <div className="category-item">
+                            {
+                                categoryData.map(
+                                    data => <Link
+                                        key={data}
+                                        to={`/${data}`}
+                                        data={data}
+                                    >{data}</Link>
+                                )
+                            }
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
