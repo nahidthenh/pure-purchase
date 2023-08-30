eef6a06bebd9155defdcdb6c6e0dc0184160378b
nahidthenh <nhgroup18@gmail.com>
2023-08-30 22:29:15 +0600

    Sidebar widget

--- src/components/widget/Discount.jsx
+++ src/components/widget/Discount.jsx
@@ -11,33 +11,33 @@ const Discount = () => {
                 <div className="d-flex flex-column gap-2 filter-check" id="discount-filter">
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="50" id="productdiscountRadio6" />
-                        <label className="form-check-label" htmlFor="productdiscountRadio6">50% or more</label>
+                        <label className="form-check-label text-lowercase" htmlFor="productdiscountRadio6">50% or more</label>
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="40" id="productdiscountRadio5" />
-                        <label className="form-check-label" htmlFor="productdiscountRadio5">40% or more</label>
+                        <label className="form-check-label text-lowercase" htmlFor="productdiscountRadio5">40% or more</label>
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="30" id="productdiscountRadio4" />
-                        <label className="form-check-label" htmlFor="productdiscountRadio4">
+                        <label className="form-check-label text-lowercase" htmlFor="productdiscountRadio4">
                             30% or more
                         </label>
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="20" id="productdiscountRadio3" />
-                        <label className="form-check-label" htmlFor="productdiscountRadio3">
+                        <label className="form-check-label text-lowercase" htmlFor="productdiscountRadio3">
                             20% or more
                         </label>
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="10" id="productdiscountRadio2" />
-                        <label className="form-check-label" htmlFor="productdiscountRadio2">
+                        <label className="form-check-label text-lowercase" htmlFor="productdiscountRadio2">
                             10% or more
                         </label>
                     </div>
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="0" id="productdiscountRadio1" />
-                        <label className="form-check-label" htmlFor="productdiscountRadio1">
+                        <label className="form-check-label text-lowercase" htmlFor="productdiscountRadio1">
                             Less than 10%
                         </label>
                     </div>
